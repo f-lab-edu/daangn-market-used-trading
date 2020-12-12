e7f61a54325c6022fa37e92f82e115f25dca5243
ssibongee <ssibongee@gmail.com>
2020-12-13 05:42:18 +0900

    [#1] 프로젝트 생성

---
+++ src/test/java/com/ssibongee/daangnmarket/DaangnmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssibongee.daangnmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
