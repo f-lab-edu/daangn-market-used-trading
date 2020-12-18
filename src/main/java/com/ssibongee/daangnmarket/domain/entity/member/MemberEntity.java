1de63540fceac6dfcb2e6c2e40f8518d4951be89
ssibongee <ssibongee@gmail.com>
2020-12-18 22:14:15 +0900

    [#2] 회원 엔티티 생성

---
+++ src/main/java/com/ssibongee/daangnmarket/domain/entity/member/MemberEntity.java
@@ -0,0 +1,35 @@
+package com.ssibongee.daangnmarket.domain.entity.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "MEMBER")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+
+    @NotEmpty
+    @Email(message = "유효하지 않은 이메일 형식입니다.",
+            regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    private String email;
+
+    @NotEmpty
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
+    private String password;
+
+    @NotEmpty
+    private String nickname;
+}
