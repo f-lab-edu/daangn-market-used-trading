467b5fefd4d50b954a6f8de1f020abde88996d87
ssibongee <ssibongee@gmail.com>
2020-12-18 22:16:59 +0900

    [#2] 회원 서비스 생성 및 회원가입 기능 구현

---
+++ src/main/java/com/ssibongee/daangnmarket/service/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.ssibongee.daangnmarket.service.member;
+
+import com.ssibongee.daangnmarket.domain.entity.member.MemberEntity;
+
+public interface MemberService {
+
+    public void registrationMember(MemberEntity memberEntity);
+}
---
+++ src/main/java/com/ssibongee/daangnmarket/service/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ssibongee.daangnmarket.service.member;
+
+import com.ssibongee.daangnmarket.domain.entity.member.MemberEntity;
+import com.ssibongee.daangnmarket.domain.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void registrationMember(MemberEntity memberEntity) {
+        memberRepository.save(memberEntity);
+    }
+}
