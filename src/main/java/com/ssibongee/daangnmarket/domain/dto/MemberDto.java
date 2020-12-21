ca3eed344e836be4336558400e26bc8e2a0225fd
ssibongee <ssibongee@gmail.com>
2020-12-21 19:36:54 +0900

    [#2] MemberDto 클래스 생성 및 Member Entity 리팩토링

--- src/main/java/com/ssibongee/daangnmarket/domain/dto/MemberDto.java
+++ src/main/java/com/ssibongee/daangnmarket/domain/dto/MemberDto.java
@@ -1,35 +1,36 @@
-package com.ssibongee.daangnmarket.domain.entity.member;
+package com.ssibongee.daangnmarket.domain.dto;
 
+import com.ssibongee.daangnmarket.domain.entity.Member;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-@Entity
 @Getter
-@Setter
-@Table(name = "MEMBER")
-public class Member {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "MEMBER_ID")
-    private Long id;
-
+@Builder
+public class MemberDto {
 
     @NotEmpty
     @Email(message = "유효하지 않은 이메일 형식입니다.",
             regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
-    private String email;
+    private final String email;
 
     @NotEmpty
     @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
             regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
-    private String password;
+    private final String password;
 
     @NotEmpty
-    private String nickname;
+    private final String nickname;
+
+    public static Member toEntity(MemberDto memberDto, PasswordEncoder passwordEncoder) {
+        return Member.builder()
+                .email(memberDto.getEmail())
+                .password(passwordEncoder.encode(memberDto.getPassword()))
+                .nickname(memberDto.getNickname())
+                .build();
+    }
 }
--- src/main/java/com/ssibongee/daangnmarket/service/member/GeneralMemberService.java
+++ src/main/java/com/ssibongee/daangnmarket/service/member/GeneralMemberService.java
@@ -1,9 +1,10 @@
 package com.ssibongee.daangnmarket.service.member;
 
-import com.ssibongee.daangnmarket.domain.entity.member.Member;
+import com.ssibongee.daangnmarket.domain.entity.Member;
 import com.ssibongee.daangnmarket.domain.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +13,7 @@ public class GeneralMemberService implements MemberService {
     private final MemberRepository memberRepository;
 
     @Override
+    @Transactional
     public void registrationMember(Member member) {
         memberRepository.save(member);
     }
