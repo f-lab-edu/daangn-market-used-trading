d004614f67673edc8a17cffe4d0c0b1a0f5ac691
ssibongee <ssibongee@gmail.com>
2020-12-25 19:23:55 +0900

    [#3] Redis 세션을 위한 설정 추가

--- src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
+++ src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
@@ -2,8 +2,10 @@ package com.ssibongee.daangnmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class DaangnmarketApplication {
 
 	public static void main(String[] args) {
