f2d1068caf71bd78f0d631f1955cec5ad6ba48c3
ssibongee <ssibongee@gmail.com>
2020-12-25 19:23:13 +0900

    [#3] 이메일을 이용한 사용자 검색 기능 추가

--- src/main/java/com/ssibongee/daangnmarket/service/member/GeneralMemberService.java
+++ src/main/java/com/ssibongee/daangnmarket/service/member/GeneralMemberService.java
@@ -1,10 +1,12 @@
 package com.ssibongee.daangnmarket.service.member;
 
+import com.ssibongee.daangnmarket.advice.exception.MemberNotFoundException;
 import com.ssibongee.daangnmarket.domain.entity.Member;
 import com.ssibongee.daangnmarket.domain.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +23,10 @@ public class GeneralMemberService implements MemberService {
     @Override
     public boolean isDuplicatedEmail(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    @Override
+    public Member findMemberByEmail(String email) {
+        return memberRepository.findMemberByEmail(email).orElseThrow(MemberNotFoundException::new);
     }
 }
--- src/main/java/com/ssibongee/daangnmarket/service/member/MemberService.java
+++ src/main/java/com/ssibongee/daangnmarket/service/member/MemberService.java
@@ -3,9 +3,12 @@ package com.ssibongee.daangnmarket.service.member;
 
 import com.ssibongee.daangnmarket.domain.entity.Member;
 
+
 public interface MemberService {
 
     public void registrationMember(Member member);
 
     public boolean isDuplicatedEmail(String email);
+
+    public Member findMemberByEmail(String email);
 }
