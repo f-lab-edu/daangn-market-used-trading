542a814d1b87d138a59a335c90718dd6f473c7fb
ssibongee <ssibongee@gmail.com>
2020-12-28 17:07:04 +0900

    [#3] 서비스 레이어에서 로그인 처리 및 사용자 유효성 검사 로직을 수행하도록 수정

--- src/main/java/com/ssibongee/daangnmarket/controller/MemberController.java
+++ src/main/java/com/ssibongee/daangnmarket/controller/MemberController.java
@@ -2,7 +2,9 @@ package com.ssibongee.daangnmarket.controller;
 
 import com.ssibongee.daangnmarket.domain.dto.MemberDto;
 import com.ssibongee.daangnmarket.domain.entity.Member;
+import com.ssibongee.daangnmarket.service.member.LoginService;
 import com.ssibongee.daangnmarket.service.member.MemberService;
+import com.ssibongee.daangnmarket.service.member.SessionLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import static com.ssibongee.daangnmarket.commons.HttpStatusResponseEntity.*;
@@ -21,9 +22,9 @@ import static com.ssibongee.daangnmarket.commons.HttpStatusResponseEntity.*;
 @RequestMapping("/api/members")
 public class MemberController {
 
-    private static final String MEMBER_ID = "MEMBER_ID";
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
+    private final LoginService loginService;
 
     /**
      * 사용자 회원가입 기능
@@ -33,6 +34,13 @@ public class MemberController {
      */
     @PostMapping
     public ResponseEntity<HttpStatus> registration(@RequestBody @Valid MemberDto memberDto) {
+
+        // 클라이언트에서 사용자 이메일 중복체크를 수행하지만 API요청에 의한 예외상황에 대비하여 더블체크
+        boolean isDuplicated = memberService.isDuplicatedEmail(memberDto.getEmail());
+
+        if(isDuplicated) {
+            return RESPONSE_CONFLICT;
+        }
 
         Member member = MemberDto.toEntity(memberDto, passwordEncoder);
         memberService.registrationMember(member);
@@ -60,30 +68,29 @@ public class MemberController {
      * 사용자 로그인 기능
      *
      * @param memberDto
-     * @param httpSession
      * @return
      */
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> login(@RequestBody @Valid MemberDto memberDto, HttpSession httpSession) {
+    public ResponseEntity<HttpStatus> login(@RequestBody @Valid MemberDto memberDto) {
 
-        Member member = memberService.findMemberByEmail(memberDto.getEmail());
+        boolean isValidMember = memberService.isValidMember(memberDto, passwordEncoder);
 
-        if (passwordEncoder.matches(memberDto.getPassword(), member.getPassword())) {
-            httpSession.setAttribute(MEMBER_ID, member.getId());
+        if (isValidMember) {
             return RESPONSE_OK;
         }
+
         return RESPONSE_BAD_REQUEST;
     }
 
     /**
      * 사용자 로그아웃 기능
      *
-     * @param httpSession
      * @return
      */
     @GetMapping("/logout")
-    public ResponseEntity<HttpStatus> logout(HttpSession httpSession) {
-        httpSession.removeAttribute(MEMBER_ID);
+    public ResponseEntity<HttpStatus> logout() {
+        loginService.logout();
         return RESPONSE_OK;
     }
+
 }
