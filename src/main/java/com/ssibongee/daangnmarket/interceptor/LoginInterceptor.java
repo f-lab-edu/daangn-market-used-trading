16755530f90cdd6423d491ba03748a18eeea4189
ssibongee <ssibongee@gmail.com>
2021-01-01 23:01:37 +0900

    [#4] 로그인 체크를 위한 인터셉터 구현

---
+++ src/main/java/com/ssibongee/daangnmarket/interceptor/LoginInterceptor.java
@@ -0,0 +1,28 @@
+package com.ssibongee.daangnmarket.interceptor;
+
+import com.ssibongee.daangnmarket.advice.exception.MemberNotFoundException;
+import com.ssibongee.daangnmarket.advice.exception.UnAuthorizedAccessException;
+import com.ssibongee.daangnmarket.commons.annotation.LoginRequired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    private static final String MEMBER_ID = "MEMBER_ID";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        String memberId = (String) request.getSession().getAttribute(MEMBER_ID);
+        if (handlerMethod.hasMethodAnnotation(LoginRequired.class) && memberId == null) {
+            throw new UnAuthorizedAccessException();
+        }
+        return true;
+    }
+}
