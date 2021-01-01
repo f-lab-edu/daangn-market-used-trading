60fb543dc7a03cb1a1736601f8e8087cd218db41
ssibongee <ssibongee@gmail.com>
2021-01-01 23:07:04 +0900

    [#4] 로그인 체크 인터셉터를 등록

---
+++ src/main/java/com/ssibongee/daangnmarket/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ssibongee.daangnmarket.config;
+
+import com.ssibongee.daangnmarket.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+}
