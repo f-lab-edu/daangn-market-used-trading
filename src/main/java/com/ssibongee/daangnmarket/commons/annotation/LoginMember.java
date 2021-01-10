70cc9669b0924199e9756cbf669e390a49b5e488
ssibongee <ssibongee@gmail.com>
2021-01-10 16:05:05 +0900

    [#4] 로그인 된 사용자 정를 가져오기 위한 커스텀 ArgumentResolver 구현

---
+++ src/main/java/com/ssibongee/daangnmarket/commons/annotation/LoginMember.java
@@ -0,0 +1,9 @@
+package com.ssibongee.daangnmarket.commons.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LoginMember {
+}
--- src/main/java/com/ssibongee/daangnmarket/config/WebConfig.java
+++ src/main/java/com/ssibongee/daangnmarket/config/WebConfig.java
@@ -1,19 +1,29 @@
 package com.ssibongee.daangnmarket.config;
 
 import com.ssibongee.daangnmarket.interceptor.LoginInterceptor;
+import com.ssibongee.daangnmarket.resolver.LoginMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginInterceptor loginInterceptor;
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginMemberArgumentResolver);
     }
 }
