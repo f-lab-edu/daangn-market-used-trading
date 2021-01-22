9716eb850f39d547bc304761b93df521f557cd15
ssibongee <ssibongee@gmail.com>
2021-01-23 00:23:34 +0900

    [#10] 중복 선언된 상수를 제거하고 기존에 선언된 상수를 사용하도록 수정

--- src/test/java/com/ssibongee/daangnmarket/service/member/LoginServiceTest.java
+++ src/test/java/com/ssibongee/daangnmarket/service/member/LoginServiceTest.java
@@ -3,8 +3,6 @@ package com.ssibongee.daangnmarket.service.member;
 import com.ssibongee.daangnmarket.advice.exception.MemberNotFoundException;
 import com.ssibongee.daangnmarket.domain.dto.MemberDto;
 import com.ssibongee.daangnmarket.domain.entity.Member;
-import com.ssibongee.daangnmarket.domain.repository.member.MemberRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static com.ssibongee.daangnmarket.service.member.SessionLoginService.MEMBER_ID;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -28,14 +27,7 @@ class LoginServiceTest {
     @Mock
     private GeneralMemberService memberService;
 
-    @Mock
-    private MemberRepository memberRepository;
-
     private MockHttpSession mockHttpSession;
-
-    private static final String MEMBER_ID = "MEMBER_ID";
-
-    private static final Long LOGIN_MEMBER_ID = 1L;
 
     private Member member;
 
@@ -58,7 +50,7 @@ class LoginServiceTest {
     @DisplayName("사용자가 로그인 성공하는 경우 세션에 사용자 아이디를 저장한다.")
     void successToLogin() {
         // when
-        loginService.login(LOGIN_MEMBER_ID);
+        loginService.login(1L);
 
         // then
         assertThat(mockHttpSession.getAttribute(MEMBER_ID)).isNotNull();
@@ -93,7 +85,7 @@ class LoginServiceTest {
     @DisplayName("사용자가 로그아웃에 성공하는 경우 세션에 저장된 사용자 아이디를 제거한다.")
     void successToLogout() {
         // given
-        mockHttpSession.setAttribute(MEMBER_ID, LOGIN_MEMBER_ID);
+        mockHttpSession.setAttribute(MEMBER_ID, 1L);
 
         // when
         loginService.logout();
@@ -106,7 +98,7 @@ class LoginServiceTest {
     @DisplayName("사용자가 로그인된 상태이면 세션에 저장된 사용자 아이디를 통해 사용자를 조회한다.")
     void isExistLoginMember() {
         // given
-        mockHttpSession.setAttribute(MEMBER_ID, LOGIN_MEMBER_ID);
+        mockHttpSession.setAttribute(MEMBER_ID, 1L);
         when(memberService.findMemberById(anyLong())).thenReturn(member);
 
         // when
