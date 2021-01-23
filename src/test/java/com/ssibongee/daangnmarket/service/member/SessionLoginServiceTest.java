635f42d25e8a7fd089593f9bacc362fc22d4ec8a
ssibongee <ssibongee@gmail.com>
2021-01-23 23:20:09 +0900

    [#10] 로그인 테스트 대신 세션 로그인 테스트라는 명확한 이름을 가지도록 수정

--- src/test/java/com/ssibongee/daangnmarket/service/member/SessionLoginServiceTest.java
+++ src/test/java/com/ssibongee/daangnmarket/service/member/SessionLoginServiceTest.java
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class LoginServiceTest {
+class SessionLoginServiceTest {
 
     private SessionLoginService loginService;
 
