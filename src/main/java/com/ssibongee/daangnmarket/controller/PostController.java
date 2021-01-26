67d84ffa60cbc2dab55f74c66bedabd7aaf06fa8
ssibongee <ssibongee@gmail.com>
2021-01-27 07:17:41 +0900

    [#14] 거래 게시물 등록 기능 구현

---
+++ src/main/java/com/ssibongee/daangnmarket/controller/PostController.java
@@ -0,0 +1,37 @@
+package com.ssibongee.daangnmarket.controller;
+
+
+import com.ssibongee.daangnmarket.commons.annotation.LoginMember;
+import com.ssibongee.daangnmarket.commons.annotation.LoginRequired;
+import com.ssibongee.daangnmarket.domain.dto.PostCreateRequest;
+import com.ssibongee.daangnmarket.domain.entity.Member;
+import com.ssibongee.daangnmarket.service.post.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.ssibongee.daangnmarket.commons.HttpStatusResponseEntity.RESPONSE_OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @LoginRequired
+    @PostMapping("/")
+    public ResponseEntity<HttpStatus> createNewPost(@RequestBody @Valid PostCreateRequest postCreateRequest,
+                                                    @LoginMember Member member) {
+
+        postService.createNewPost(postCreateRequest, member);
+
+        return RESPONSE_OK;
+    }
+}
