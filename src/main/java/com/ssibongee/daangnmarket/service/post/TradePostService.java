9d98bc7523a70618b548b07bb13a28c0f259dfd7
ssibongee <ssibongee@gmail.com>
2021-02-01 12:53:29 +0900

    [#14] 새로운 거래 게시물 등록시 카테고리를 데이터베이스에서 조회하도록 수정

--- src/main/java/com/ssibongee/daangnmarket/service/post/TradePostService.java
+++ src/main/java/com/ssibongee/daangnmarket/service/post/TradePostService.java
@@ -1,8 +1,11 @@
 package com.ssibongee.daangnmarket.service.post;
 
+import com.ssibongee.daangnmarket.advice.exception.CategoryNotFoundException;
 import com.ssibongee.daangnmarket.domain.dto.PostCreateRequest;
+import com.ssibongee.daangnmarket.domain.entity.Category;
 import com.ssibongee.daangnmarket.domain.entity.Member;
 import com.ssibongee.daangnmarket.domain.entity.Post;
+import com.ssibongee.daangnmarket.domain.repository.post.CategoryRepository;
 import com.ssibongee.daangnmarket.domain.repository.post.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class TradePostService implements PostService {
 
     private final PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
 
     @Override
     @Transactional
     public void createNewPost(PostCreateRequest postCreateRequest, Member member) {
+
         Post post = postCreateRequest.toEntity(member);
+        Category category = categoryRepository.findCategoryByCategoryName(
+                postCreateRequest.getCategory()).orElseThrow (() -> new CategoryNotFoundException(postCreateRequest.getCategory()));
+
+        post.setCategory(category);
+
         postRepository.save(post);
     }
 }
