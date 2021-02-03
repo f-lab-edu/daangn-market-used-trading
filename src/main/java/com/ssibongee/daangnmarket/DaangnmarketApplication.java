7deccc7e4d19ecd2bcd63b0c45e4033b868b1ff5
ssibongee <ssibongee@gmail.com>
2021-02-03 18:03:08 +0900

    [#22] 캐싱 기능을 사용하기 위해 @EnableCaching 어노테이션 추가

--- src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
+++ src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
@@ -2,11 +2,13 @@ package com.ssibongee.daangnmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 @EnableRedisHttpSession
 public class DaangnmarketApplication {
 
