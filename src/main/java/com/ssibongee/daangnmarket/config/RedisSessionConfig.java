434179289349736ae185f591c697180f3c4ddff2
ssibongee <ssibongee@gmail.com>
2021-02-03 17:57:13 +0900

    [#22] RedisConnectionFactory 타입의 빈이 두 개가 존재하므로 @Qualifier 설정을 통한 명시적인 빈 주입

--- src/main/java/com/ssibongee/daangnmarket/config/RedisSessionConfig.java
+++ src/main/java/com/ssibongee/daangnmarket/config/RedisSessionConfig.java
@@ -1,8 +1,10 @@
 package com.ssibongee.daangnmarket.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,15 +21,16 @@ public class RedisSessionConfig {
     @Value("${spring.redis.session.port}")
     private int port;
 
-    @Bean
-    public RedisConnectionFactory redisSessionConnectionFactory() {
+    @Primary
+    @Bean(name = "redisSessionConnectionFactory")
+    public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("redisSessionConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisSessionConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
