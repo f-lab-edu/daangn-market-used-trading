081644b4647f8602f12fa5d0ef2ca003702edf57
ssibongee <ssibongee@gmail.com>
2021-02-03 18:08:57 +0900

    [#22] Category 엔티티의 기본생성자 생성

--- src/main/java/com/ssibongee/daangnmarket/domain/entity/Category.java
+++ src/main/java/com/ssibongee/daangnmarket/domain/entity/Category.java
@@ -1,11 +1,14 @@
 package com.ssibongee.daangnmarket.domain.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 
     @Id
