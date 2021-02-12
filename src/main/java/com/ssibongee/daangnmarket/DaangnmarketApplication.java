bb0b5485b087c697b2f077c40f07635359d9bf2e
ssibongee <ssibongee@gmail.com>
2021-02-13 00:11:00 +0900

    [#22] LocalDateTime.class 직렬화를 위한 ObjectMapper 정의

--- src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
+++ src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
@@ -2,13 +2,11 @@ package com.ssibongee.daangnmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableCaching
 @EnableRedisHttpSession
 public class DaangnmarketApplication {
 
--- src/main/java/com/ssibongee/daangnmarket/config/RedisCacheConfig.java
+++ src/main/java/com/ssibongee/daangnmarket/config/RedisCacheConfig.java
@@ -1,7 +1,12 @@
 package com.ssibongee.daangnmarket.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,7 +17,9 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+
 @Configuration
+@EnableCaching
 public class RedisCacheConfig {
 
     @Value("${spring.redis.cache.host}")
@@ -21,18 +28,33 @@ public class RedisCacheConfig {
     @Value("${spring.redis.cache.port}")
     private int port;
 
+    @Bean(name = "redisObjectMapper")
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.activateDefaultTyping(BasicPolymorphicTypeValidator
+                        .builder()
+                        .allowIfSubType(Object.class)
+                        .build()
+                , ObjectMapper.DefaultTyping.EVERYTHING);
+        objectMapper.registerModules(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        return objectMapper;
+    }
+
     @Bean(name = "redisCacheConnectionFactory")
     public RedisConnectionFactory redisCacheConnectionFactory() {
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
     }
 
     @Bean
-    public RedisCacheManager redisCacheManager(@Qualifier("redisCacheConnectionFactory")
-                                                               RedisConnectionFactory redisConnectionFactory) {
+    public RedisCacheManager redisCacheManager(
+            @Qualifier("redisCacheConnectionFactory") RedisConnectionFactory redisConnectionFactory,
+            @Qualifier("redisObjectMapper") ObjectMapper objectMapper) {
 
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
 
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(redisCacheConfiguration)
