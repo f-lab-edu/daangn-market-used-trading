1a131005fafc839ba699ab7ab64cc4ffa9eccd80
ssibongee <ssibongee@gmail.com>
2021-02-13 00:24:50 +0900

    [#22] Jackson의 ObjectMapper를 적용하기 위해 기본생성자를 가지도록 수정

--- src/main/java/com/ssibongee/daangnmarket/domain/dto/MemberDto.java
+++ src/main/java/com/ssibongee/daangnmarket/domain/dto/MemberDto.java
@@ -1,8 +1,10 @@
 package com.ssibongee.daangnmarket.domain.dto;
 
 import com.ssibongee.daangnmarket.domain.entity.Member;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.validation.constraints.Email;
@@ -11,20 +13,22 @@ import javax.validation.constraints.Pattern;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDto {
 
     @NotEmpty
     @Email(message = "유효하지 않은 이메일 형식입니다.",
             regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
-    private final String email;
+    private String email;
 
     @NotEmpty
     @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
             regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
-    private final String password;
+    private String password;
 
     @NotEmpty
-    private final String nickname;
+    private String nickname;
 
     public static Member toEntity(MemberDto memberDto, PasswordEncoder passwordEncoder) {
         return Member.builder()
