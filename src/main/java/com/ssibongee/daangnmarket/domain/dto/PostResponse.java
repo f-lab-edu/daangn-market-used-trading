3ee42b54bd2216a4f2541a113e4f5033c88d5c4c
ssibongee <ssibongee@gmail.com>
2021-02-12 23:37:57 +0900

    [#22] 중복으로 나타나는 직렬화 어노테이션을 제거

--- src/main/java/com/ssibongee/daangnmarket/domain/dto/PostResponse.java
+++ src/main/java/com/ssibongee/daangnmarket/domain/dto/PostResponse.java
@@ -1,9 +1,5 @@
 package com.ssibongee.daangnmarket.domain.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.ssibongee.daangnmarket.domain.entity.Address;
 import com.ssibongee.daangnmarket.domain.entity.Location;
 import com.ssibongee.daangnmarket.domain.entity.Post;
@@ -29,11 +25,7 @@ public class PostResponse {
     private Address address;
     private Location location;
 
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdTime;
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedTime;
 
 
