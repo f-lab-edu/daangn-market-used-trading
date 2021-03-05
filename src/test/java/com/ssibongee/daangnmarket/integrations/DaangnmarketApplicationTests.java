ac9804e0a6dbdc89d78abacf67532e6d93212adb
ssibongee <ssibongee@gmail.com>
2021-03-06 00:45:31 +0900

    [#24] 통합 테스트 패키지 변경 및 단위 테스트 수행시 통합 테스트 예외 케이스 추가

--- src/test/java/com/ssibongee/daangnmarket/integrations/DaangnmarketApplicationTests.java
+++ src/test/java/com/ssibongee/daangnmarket/integrations/DaangnmarketApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ssibongee.daangnmarket;
+package com.ssibongee.daangnmarket.integrations;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
