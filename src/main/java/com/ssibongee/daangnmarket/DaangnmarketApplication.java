c18548ca9148a9a66433eb2a8b9bd2fe86e50fca
ssibongee <ssibongee@gmail.com>
2021-03-12 09:08:04 +0900

    [#40] 통합테스트 과정에서 어노테이션으로 인한 충돌을 방지하기 위해 각각을 별도의 설정 클래스에서 등록하도록 분리

--- src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
+++ src/main/java/com/ssibongee/daangnmarket/DaangnmarketApplication.java
@@ -2,12 +2,8 @@ package com.ssibongee.daangnmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableRedisHttpSession
 public class DaangnmarketApplication {
 
 	public static void main(String[] args) {
