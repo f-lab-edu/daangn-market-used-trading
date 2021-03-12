bed4544955dc86cbededaf1f9456b57c30da1b3b
ssibongee <ssibongee@gmail.com>
2021-03-12 09:13:40 +0900

    [#40] 반복적으로 사용되는 테스트를 위한 사용자 엔티티 및 DTO에 대해서 Fixture 클래스를 생성하여 상수로 정의

---
+++ src/test/java/com/ssibongee/daangnmarket/fixture/MemberFixture.java
@@ -0,0 +1,39 @@
+package com.ssibongee.daangnmarket.fixture;
+
+import com.ssibongee.daangnmarket.member.domain.entity.Member;
+import com.ssibongee.daangnmarket.member.dto.MemberDto;
+
+public class MemberFixture {
+
+    public static final Member ADMIN_MEMBER = new Member(
+            "admin@daangnmarket.com",
+            "!Admin123",
+            "admin"
+    );
+
+    public static final Member MEMBER1 = new Member(
+            "member1@daangnmarket.com",
+            "!Member123",
+            "user1"
+    );
+
+    public static final Member MEMBER2 = new Member(
+            "member2@daangnmarket.com",
+            "!Member123",
+            "user2"
+    );
+
+    public static final MemberDto MEMBER_REGISTRATION_REQUEST = new MemberDto(
+            "newmember@daangnmarket.com",
+            "!Newmember123",
+            "newMember"
+    );
+
+    public static final Member NEW_MEMBER = new Member(
+            "newmember@daangnmarket.com",
+            "!Newmember123",
+            "newMember"
+    );
+
+
+}
