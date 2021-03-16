cd8f001a3664ebe915ed3077a1f41e0a351c9f89
ssibongee <ssibongee@gmail.com>
2021-03-16 16:38:39 +0900

    [#48] ProfileResponse 클래스에서 반환시 불필요한 필드 값 제거

--- src/main/java/com/ssibongee/daangnmarket/member/dto/ProfileResponse.java
+++ src/main/java/com/ssibongee/daangnmarket/member/dto/ProfileResponse.java
@@ -10,13 +10,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProfileResponse {
 
-    private final Long id;
     private final String email;
     private final String nickname;
 
     public static ProfileResponse of(Member member) {
         return ProfileResponse.builder()
-                .id(member.getId())
                 .email(member.getEmail())
                 .nickname(member.getNickname())
                 .build();
